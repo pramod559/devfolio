a60170dec8a654fa79244036b9fcd9404195af84
pramod <kumarpramod559@gmail.com>
2023-12-21 22:53:00 +0530

    start app

---
+++ folio/lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Pramod kumar, a Flutter developer, Technical blog writer and UI designer.';
+
+  static const String aboutMeDetail =
+      'I\'m a Fresh Graduate with Electronics & Communication  as Majors from Punjab Technical University, Punjab. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a former  Google Developer Student Clubs (DSC) lead and also CEO/Co-Founder Flutter Islamabad, Pakistan. Currently working as Associate Software Engineer at Sastaticket.pk';
+}
